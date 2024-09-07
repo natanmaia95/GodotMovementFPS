75038935741c794e9677f0b473179193ed9fdd29
LoboGuarah <feitosacontato@gmail.com>
2024-09-07 03:12:56 -0300

    Stage 1 extended. Stage 2 scene created

--- scenes/objects/stage/finish_stage_area.gd
+++ scenes/objects/stage/finish_stage_area.gd
@@ -1,5 +1,7 @@
 class_name FinishStageArea
 extends PlayerDetectorComponent
+var freeze_timer = Timer.new()
 
 func on_player_entered(_player:CharacterBody3D) -> void:
 	if StageManager.finish_stage(): disabled = true
+	get_tree().paused = true
