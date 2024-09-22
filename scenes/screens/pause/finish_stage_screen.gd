5667fea5507f8a55901c15eeb86375117b83fdff
Natan Maia <natanmaiaplays@outlook.com>
2024-09-22 14:07:53 -0300

    finish screen

--- scenes/screens/pause/finish_stage_screen.gd
+++ scenes/screens/pause/finish_stage_screen.gd
@@ -13,6 +13,7 @@ func _input(event):
 func open():
 	visible = true
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	build_components()
 	%BtnRestart.grab_focus()
 	pass
 
@@ -22,13 +23,26 @@ func close():
 	pass
 
 
+
+func build_components() -> void:
+	#results label
+	$RichTextLabel.text = "Time: %s %s" % [
+		StageManager.timer.as_string(),
+		"(NEW RECORD)" if StageManager.current_stage_results["improved_time"] else ""
+	]
+	$RichTextLabel.text += "\nScore: %d %s" % [
+		StageManager.current_stage_results["new_record"]["score"],
+		"(NEW RECORD)" if StageManager.current_stage_results["improved_score"] else ""
+	]
+
+
+
+
 func _on_stage_finished():
 	self._open()
 
-
 func _on_btn_restart_pressed():
 	StageManager.reset_stage()
-
 
 func _on_btn_exit_pressed():
 	StageManager.exit_stage()
